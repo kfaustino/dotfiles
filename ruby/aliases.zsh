3b5133f2bfe0a7b3be9ebf42735961b6499b19c6
Kevin Faustino <kevin@remarkablelabs.com>
2014-03-16 17:48:18 -0400

    Add Rails specific aliases for Rails 4.1

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,5 @@
 alias be="bundle exec"
-alias migrate='rake db:migrate db:test:clone'
+alias migrate='rake db:migrate'
+alias schema='RAILS_ENV=test rake db:schema:load'
 alias r="bin/rails"
+alias spec="bin/spring rspec"
