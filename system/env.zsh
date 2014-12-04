aca43df5a2d4cf92521b2e1a10062a4e9b6b3b0e
Kevin Faustino <kevin@remarkablelabs.com>
2014-12-04 15:08:22 -0500

    Set default editor to Atom

--- system/env.zsh
+++ system/env.zsh
@@ -1,6 +1,4 @@
 # Only set this if we haven't set $EDITOR up somewhere else previously.
 if [[ "$EDITOR" == "" ]] ; then
-  export EDITOR="mate -w"
+  export EDITOR="/usr/local/bin/atom --wait"
 fi
-
-
