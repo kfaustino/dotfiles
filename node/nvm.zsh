bfc54179fc90123f730fdab47c70a8f71edcb748
Kevin Faustino <kevin@remarkablelabs.com>
2016-12-14 12:11:15 -0500

    Use nvm to manage node versions

---
+++ node/nvm.zsh
@@ -0,0 +1,3 @@
+export NVM_DIR="$HOME/.nvm"
+
+. "/usr/local/opt/nvm/nvm.sh"
--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,1 @@
-export NODE_PATH="/usr/local/lib/node_modules"
-export PATH=/usr/local/heroku/bin:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$PATH
+export PATH=/usr/local/heroku/bin:/usr/local/bin:/usr/local/sbin:$PATH
